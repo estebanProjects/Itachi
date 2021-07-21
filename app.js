ee4f1d5067d3b30177716bfb903b5d82e82cb21b
Esteban Espinoza <estebanespinoza1905@gmail.com>
2021-07-21 14:07:33 -0500

    Declaracion de las variables principales en el js

---
+++ app.js
@@ -0,0 +1,8 @@
+let imagen_itachi = document.getElementById('imagen_de_itachi')
+let view_width = document.getElementById('width')
+let view_height = document.getElementById('height')
+
+let width = 185
+let height = 302
+let indice_de_crecimiento_width = 6.2
+let indice_de_crecimiento_height = 10.12
