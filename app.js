b3e38ac420270930b49d63ddf610ed22c612d7c6
Esteban Espinoza <estebanespinoza1905@gmail.com>
2021-07-21 14:09:02 -0500

    Agregando funcionalidad de la funcion crecer_imagen

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ let width = 185
 let height = 302
 let indice_de_crecimiento_width = 6.2
 let indice_de_crecimiento_height = 10.12
+
+imagen_itachi.addEventListener('click', crecer_imagen)
+
+function crecer_imagen() {
+    height = height + indice_de_crecimiento_height
+    imagen_itachi.style.height = height + "px"
+
+    width = width + indice_de_crecimiento_width
+    imagen_itachi.style.width = width + "px"
+}
